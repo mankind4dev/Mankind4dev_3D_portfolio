3fccabe8ddec21ed91be96131fb50a6b9148e279
mankind4dev <ogunmakin120@gmail.com>
2023-12-13 23:25:37 +0100

    My first portfolio commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  const date = new Date()
+  return (
+    <div className=' bg-primary'>
+      <p className="text-center text-[20px] font-sans py-2"><span className="text-[#FFD700]">Mankind</span><span className="text-blue-500">4dev</span>. All rights Reserved. &copy; {date.getFullYear()}</p>
+    </div>
+  )
+}
