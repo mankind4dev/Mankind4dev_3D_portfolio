5cc357c8abc69dc79811d2988af97141c51ff335
mankind4dev <ogunmakin120@gmail.com>
2024-02-04 11:56:49 +0100

    update

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Tech from "./Tech";
 import Experience from "./Experience";
 import Works from "./Works";
 import Feedbacks from "./Feedbacks";
-import COntact from "./COntact";
+import Contact from "./COntact";
 import CanvasLoader from "./Loader";
 import Footer from "./Footer";
 
@@ -19,7 +19,7 @@ export {
   Experience,
   Works,
   Feedbacks,
-  COntact,
+  Contact,
   CanvasLoader,
   EarthCanvas, 
   BallCanvas, 
