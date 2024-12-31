c198dc03c5dad8a8c63a0b33c0d260b7548158d8
mankind4dev <ogunmakin120@gmail.com>
2024-12-31 21:59:37 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    chunkSizeWarningLimit: 500,
   },
   server: {
     port: process.env.PORT || 3000,
+    host: true
   },
 });
